de1a8cc663ff9f01070dc74972b917c49d2ed4f2
Daniel Sollis <danielsollis2@gmail.com>
2017-11-21 20:41:52 -0800

    changes to token.js

--- server/mocks/token.js
+++ server/mocks/token.js
@@ -2,25 +2,23 @@
 'use strict';
 
 module.exports = function(app) {
-  var express = require('express');
-  var tokenRouter = express.Router();
+    const express = require('express');
+    let tokenRouter = express.Router();
 
-  tokenRouter.post('/token', function(req, res) {
-    if (req.body.grant_type === 'password') {
-        if (req.body.username === 'letme' && req.body.password === 'me') {
-            res.status(200).send('{ "access_token" : "secret token!" }');
+    tokenRouter.post('/token', function(req, res) {
+        if (req.body.grant_type === 'password') {
+            if (req.body.username === 'letme' && req.body.password === 'in') {
+                res.status(200).send('{ "access_token" : "secret token!" }');
+            } else {
+                res.status(400).send('{ "error": "invalid_grant" }');
+            }
         } else {
-            res.status(400).send('{ "error": "invalid_grant" }');
+            res.status(400).send('{ "error": "unsupported_grant_type" }');
         }
-    } else {
-        res.status(400).send('{ "error": "unsupported_grant_type" }');
-    }
-  });
-  app.use('/', tokenRouter);
+    });
+    app.use('/', tokenRouter);
 };
 
-// const express = require('express');
-// let tokenRouter = express.Router();
 //
 // tokenRouter.get('/', function(req, res) {
 //   res.send({
