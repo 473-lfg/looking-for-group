2c250430ac8b58018810791fa67ba973ebc129f7
Daniel Sollis <danielsollis2@gmail.com>
2017-11-23 17:11:25 -0800

    receiving token works

---
+++ app/components/index-page.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+import Component from '@ember/component';
+
+export default Component.extend({
+
+  authManager: Ember.inject.service('session'),
+
+  actions: {
+    authenticate() {
+      const { login, password } = this.getProperties('login', 'password');
+      this.get('authManager').authenticate('authenticator:oauth2', login, password).then(() => {
+        alert('Success! Click the top link!');
+      }, (err) => {
+        alert('Error obtaining token: ' + err.responseText);
+      });
+    },
+
+    invalidateSession() {
+        this.get('session').invalidate();
+    }
+
+  }
+});
--- server/index.js
+++ server/index.js
@@ -1,24 +1,22 @@
-/* eslint-env node */
-'use strict';
-
-// To use it create some files under `mocks/`
-// e.g. `server/mocks/ember-hamsters.js`
-//
-// module.exports = function(app) {
-//   app.get('/ember-hamsters', function(req, res) {
-//     res.send('hello');
-//   });
-// };
+const bodyParser = require('body-parser');
 
 module.exports = function(app) {
-  const globSync   = require('glob').sync;
-  const mocks      = globSync('./mocks/**/*.js', { cwd: __dirname }).map(require);
-  const proxies    = globSync('./proxies/**/*.js', { cwd: __dirname }).map(require);
 
-  // Log proxy requests
-  const morgan = require('morgan');
-  app.use(morgan('dev'));
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-  mocks.forEach(route => route(app));
-  proxies.forEach(route => route(app));
+    app.get('/api/codes', function (req, res) {
+
+        if (req.headers['authorization'] !== "Bearer success_token") {
+          return res.status(401).send('Unauthorized');
+        }
+    });
+    app.post('/token', function(req, res) {
+
+      if (req.body.username == 'login' && req.body.password == 'ok') {
+        res.send({ access_token: "success_token" });
+      } else {
+        res.status(400).send({ error: "invalid_grant" });
+      }
+
+    });
 };
